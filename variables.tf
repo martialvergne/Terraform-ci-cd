29f755c22cbf5213bc2f2641a3129e5751fbfd83
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2024-07-25 09:58:06 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "GITLAB_HOME" {
   description  = "Répertoire d'installation Gitlab"
   default      = "/home/martial/ci-cd/Terraform-ci-cd"
 }
+
+variable "url" {
+  description  = "URL d'accès à Gitlab"
+  default      = "https://192.168.1.61"
+}
